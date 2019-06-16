4068ccf9c838700329ddecffb02144d16bc748d8
小池慶彦 <ykoike@Sirius01.local>
2019-06-16 09:25:40 +0900

    completed until No.4

--- chapter01.py
+++ chapter01.py
@@ -57,11 +57,38 @@ print('04. 元素記号')
 
 a = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
 mylist = list()
+mydict = {}
+
+# 単語を空白で分割し、カンマやコロンを削除
 for i in a.split(" "):
-    mylist.append(len(i.strip('.,')))
-print(mylist) 
+    mylist.append(i.strip('.,'))
+
+# 先頭1文字のリストを作る
+num_list = [1, 5, 6, 7, 8, 9, 15, 16, 19] # コピペでHPのリストを貼り付ける
+num_list = list(map(lambda x: x - 1, num_list)) # lambda関数で全部1引いたリストを作る
+
+for i in range(len(mylist)):
+    str = mylist[i]
+    if i in num_list: # num_list番目の単語は先頭1文字
+        atom = str[0]
+    else: # それ以外は2文字
+        atom = str[0:2]
+    mydict[i+1] = atom
+print(mydict) #{1: 'H', 2: 'He', 3: 'Li', 4: 'Be', 5: 'B', 6: 'C', 7: 'N', 8: 'O', 9: 'F', 10: 'Ne', 11: 'Na', 12: 'Mi', 13: 'Al', 14: 'Si', 15: 'P', 16: 'S', 17: 'Cl', 18: 'Ar', 19: 'K', 20: 'Ca'}
+
 ## 05. n-gram
 ## 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+
+print('05. n-gram')
+
+def n_gram(sentence,n):
+    x_list = list()
+    for i in range(len(sentence)):
+        x_list.append(sentence[i:i+n])
+    print(x_list)
+
+a = 'I am an NLPer'
+n_gram(a,2)
 
 ## 06. 集合
 ## "paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
