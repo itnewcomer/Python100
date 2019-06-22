b36ff46907767ee9a3ee88aa8036172d6401042f
小池慶彦 <ykoike@Sirius01.local>
2019-06-22 22:35:36 +0900

    complete until No.14

--- chapter02.py
+++ chapter02.py
@@ -54,30 +54,35 @@ for line in fp.readlines():
     fnew1.write(my_list[0]+'\n')
     fnew2.write(my_list[1]+'\n')
 fp.close()
+fnew1.close()
+fnew2.close()
 
 # cut -f 1 -d "$(printf "\t")" hightemp.txt > col1_linux.txt 
 # cut -f 2 -d "$(printf "\t")" hightemp.txt > col2_linux.txt 
 # diff col1.txt col1_linux.txt 
 # diff col2.txt col2_linux.txt 
 
-
 ## 13. col1.txtとcol2.txtをマージ
-## 12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
+## 12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．
+## 確認にはpasteコマンドを用いよ．
 
-## 14. 先頭からN行を出力
-## 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+print("13. col1.txtとcol2.txtをマージ")
 
-## 15. 末尾のN行を出力
-## 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ．
+#f2 = open ('col2.txt', 'w')
+with open('col1.txt') as f1:
+    s1=f1.read()
+with open('col2.txt') as f2:
+    s2=f2.read()
 
-## 16. ファイルをN分割する
-## 自然数Nをコマンドライン引数などの手段で受け取り，入力のファイルを行単位でN分割せよ．同様の処理をsplitコマンドで実現せよ．
+s1 = s1.split("\n")
+s2 = s2.split("\n")
+i = 0
+merge_file = open ('merge_file.txt', 'w')
+for i in range(len(s1)-1):
+    if i < len(s1)-2:
+        merge_file.write(s1[i] + "\t" + s2[i] + "\n")
+    else:
+        merge_file.write(s1[i] + "\t" + s2[i])
+merge_file.close()
 
-## 17. １列目の文字列の異なり
-## 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
-
-## 18. 各行を3コラム目の数値の降順にソート
-## 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
-
-## 19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる
-## 各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．確認にはcut, uniq, sortコマンドを用いよ．
+# paste -d "$(printf "\t")" col1.txt col2.txt 
---
+++ chapter02_14.py
@@ -0,0 +1,18 @@
+## 14. 先頭からN行を出力
+## 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+
+print("14. 先頭からN行を出力")
+
+import sys
+line_num = 0
+line_num = int(sys.argv[2])
+i = 0
+fp = open(sys.argv[1], 'r')
+for line in fp.readlines():
+    if i < line_num:
+        print(line,end="")
+        i += 1
+    else:
+        break
+
+# head -n 3 hightemp.txt
